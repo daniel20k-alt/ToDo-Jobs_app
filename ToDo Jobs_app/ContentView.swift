2582c5c2233a1d1839c05dfc920d27414055700a
Testing_author <testing_beta@mail.com>
2020-05-01 20:03:33 +0300

    Initial Commit

---
+++ ToDo Jobs_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo Jobs_app
+//
+//  Created by DDDD on 01/05/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
